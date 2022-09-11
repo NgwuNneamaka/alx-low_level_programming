43be6b8ae04d5f0a8d7b860439ba21740e7277cf
Ngwu Nneamaka <ngwunneamakablessing@gmail.com>
2022-09-11 12:33:20 -0700

    all tasks

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point
  *
- * Return - return zero at the end 
+ * Return: return zero at the end
  */
 int main(void)
 {
@@ -10,13 +10,13 @@ int main(void)
 
 	for (i = 0; i < 100; i++)
 	{
-		for (p =0; p < 100; p++)
+		for (p = 0; p < 100; p++)
 		{
-			if ( p > i)
+			if (p > i)
 			{
 				putchar((i / 10) + '0');
 				putchar((i % 10) + '0');
-				putchar(',');
+				
 				putchar((p / 10) + '0');
 				putchar((p % 10) + '0');
 
