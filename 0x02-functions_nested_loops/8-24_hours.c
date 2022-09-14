7261ed3f2ef4e4784fb8a75d2e8529f44196e3fa
Ngwu Nneamaka <ngwunneamakablessing@gmail.com>
2022-09-14 00:51:13 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,7 +13,7 @@ void jack_bauer(void)
 	while (hours <= 23)
 	{
 		while (minutes <= 59)
-	}
+	{
 	mins_remainder = minutes % 10;
 	hours_remainder = hours % 10;
 	_putchar(hours / 10 + '0');
@@ -26,5 +26,5 @@ void jack_bauer(void)
 	}
 	hours++;
 	minutes = 0;
-}
+	}
 }
